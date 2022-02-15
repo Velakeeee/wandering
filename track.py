0de4fb6a005b71239935316140dc99ab709899ca
unknown <SENA@DOSAPRCDIFSD162.FormacionSena.RED>
2022-02-15 14:03:08 -0500

    Creamos las clases de wandering y track para el proyecto

---
+++ track.py
@@ -0,0 +1,14 @@
+class track:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+    def move(self, delt_x, delt_y):
+        return track(self.x + delt_x, self.y + delt_y)
+    
+    def distance(self, other_track):
+        delt_x = self.x - other_track.x
+        delt_y = self.y - other_track.y
+        
+        return (delt_x**2 + delt_y**2)**0.5
+        
